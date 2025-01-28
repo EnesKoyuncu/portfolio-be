f9f88807e2462a9b3bd0254746029879e4b2f0d5
Enes Ertuğrul Koyuncu <enes.koyuncu5507@gmail.com>
2025-01-28 23:31:50 +0300

    Remove sensitive infos

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+require("dotenv").config();
 
 import textRoutes from "./routes/textRoutes";
 import aboutRoutes from "./routes/abouteRoutes";
 import projectRoutes from "./routes/projectRoute";
 import blogRoutes from "./routes/blogRoutes";
 
-// 🌐 MongoDB Bağlantısı
-const mongoURI =
-  "mongodb+srv://eneskoyuncu5507:eneskoyuncu5507@portfoliocluster.8d1tw.mongodb.net/?retryWrites=true&w=majority&appName=PortfolioCluster";
+// MongoDB Connection
+const mongoURI = process.env.MONGODB_URI;
+
+if (!mongoURI) {
+  console.error("❌ MONGODB_URI is not defined in environment variables");
+  process.exit(1);
+}
+
 mongoose
   .connect(mongoURI)
   .then(() => console.log("✅ MongoDB bağlantısı başarılı!"))
@@ -18,19 +24,17 @@ mongoose
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// 🔥 Middleware'ler
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// 📌 Text Routes
+// Routes
 app.use("/api/texts", textRoutes);
-
 app.use("/api/about", aboutRoutes);
-
 app.use("/api/projects", projectRoutes);
-
 app.use("/api/blogs", blogRoutes);
-// 🌐 Sunucu Başlat
+
+// Start Server
 app.listen(PORT, () =>
   console.log(`🚀 Server is running on http://localhost:${PORT}`)
 );
